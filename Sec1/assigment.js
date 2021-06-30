2f7742aa1020cfbce0a18d4212fdfe5b877e026e
rizalraisyad <muharamrizal2@gmail.com>
2021-06-30 14:17:41 +0700

    half of section 1

---
+++ Sec1/assigment.js
@@ -0,0 +1,14 @@
+let country = 'Indonesia';
+let continent = 'Asia';
+let population = '200 milion';
+console.log(country);
+console.log(continent);
+console.log(population);
+
+const isIsland = true;
+let language;
+console.log(typeof country);
+console.log(typeof continent);
+console.log(typeof population);
+console.log(typeof isIsland);
+console.log(typeof language);
---
+++ Sec1/script.js
@@ -0,0 +1,3 @@
+let js = 'amazing';
+if (js === 'amazing') alert('javascript is FUN!');
+console.log(40 + 8 + 23 - 10);
